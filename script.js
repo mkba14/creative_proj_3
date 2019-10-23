cee84de44ad68f2350c062749cc83eefc7194988
aMakenzie Allen <allenmakenzie1427@gmail.com>
2019-10-23 20:50:16 +0000

    list of planet names now provided; 5 columns

--- script.js
+++ script.js
@@ -10,17 +10,22 @@ let app = new Vue({
             img: '',
             alt: ''
         },
-        category: 'people',
+        category: 'planets',
         index: '1',
         loading: true,
         
         addedName: '',
         addedComment: '',
         comments: {},
+        
+        planet_list: [],//, climate: "", terrain: ""},
     },
     
     created() {
         this.starwars();
+        console.log('planets');
+        this.planets();
+        
     },
     
     methods: {
@@ -29,22 +34,51 @@ let app = new Vue({
                     url = "https://swapi.co/api/";
                     this.loading = true;
                     const response = 
-                        await axios.get(url + this.category +'/'+ '1/')
+                        await axios.get(url + this.category +'/')
                             .catch(error => {this.number = this.max; 
                             });
                     console.log("response\t", response);
-                    console.log("response.name\t",response.name);
-                    console.log("json\t\t", response.json[0]);
+                    console.log("response.name\t",response.data.name);
+                    //console.log("json\t\t", response.json[0]);
                     console.log("body\t", response.body);
                 } catch (error){
                     console.log(error);
                 }
             },
            
-            addComment(){
-            },
+            async planets() {
+                console.log('planets')
+                try{
+                    url = "https://swapi.co/api/";
+                    this.loading_planets = true;
+                    let response = await axios.get(url + this.category) //+ '/' + this.index);
+                    //const numPlanets = response.data.count;
+                    while(response.data.next !== null){
+                        console.log('planets response = ')//, response.data.results);
+                        console.log(response.data.next);
+                        response = await axios.get(response.data.next);
+                        console.log(response.data.results.length)
+                        for(let i = 0; i < response.data.results.length; i++){
+                            //console.log(response.data.results[i]);//.name);
+                            console.log(response.data.results[i].name,' - ', response.data.results[i].terrain);
+                            console.log(response.data.results[i]);
+                            //this.planet_list[i].push({name: response.data.results[i].name});
+                            this.planet_list.push(response.data.results[i]);
+                            //console.log("h1",this.planet_list.name, "here")
+                        }
+                        
+                    }
+                    
+                }
+                catch (error){
+                    console.log(error);
+                }
+
+            }
             
+
         },
+        
         
         computed: {
             month() {
